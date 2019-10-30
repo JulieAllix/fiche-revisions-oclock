05751d1bda6de83221913730fbd07bb60c5d7fd3
JulieAllix <allixjulie@gmail.com>
2019-10-30 21:25:05 +0100

    ajout du lien saison 3

--- php/partials/main-nav-bar.php
+++ php/partials/main-nav-bar.php
@@ -3,5 +3,6 @@
             <!--<li><a href="accueil.php">Menu principal</a></li>-->
             <li><a href="saison1.php">Saison 1</a></li>
             <li><a href="saison2.php">Saison 2</a></li>
+            <li><a href="saison3.php">Saison 3</a></li>
         </ul>
     </nav>
