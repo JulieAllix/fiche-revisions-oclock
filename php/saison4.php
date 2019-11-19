7430b41ef2e8c194d59e8f0a8471eda53d45d8e2
JulieAllix <allixjulie@gmail.com>
2019-11-19 18:13:47 +0100

    ajout des notions du 19 11

--- php/saison4.php
+++ php/saison4.php
@@ -41,18 +41,14 @@
                             <li><em class="gras">git remove-v</em> permet de voir à qui correspndent les alias</li>
                             <li><em class="gras">git pull</em> récupérer les modifs d'un repo sur Github</li>
                             <li><em class="gras">git fetch</em> récupérer les modifs d'un repo sur Github, mais ne permet pas de faire de modifs (n'écrase pas la version précédente en local)</li>
-                            <li><em class="gras">git checkout nomDeLaBranche</em> permet de basculer sur une branche</li>
+                            <li><em class="gras">git checkout nomDeLaBranche</em> permet de basculer sur une branche/récupérer une branche</li>
                             <li><em class="gras">git branch</em> montre les branches qui existent (l'étoile indique celle sur laquelle je suis)</li>
                             <li><em class="gras">git merge nomDeLaBranche</em> fusionne la branche avec master</li>
-                            <li><em class="gras"></em></li>
+                            <li><em class="gras">git branch -a</em> permet de voir les branches dispo en local et celles dispo sur Github (en rouge)</li>
                             <li><em class="gras"></em></li>
                         </ul>
 
             </article> 
-
-        </section>
-
-        <section id="second-section">
 
             <article class="topic">
 
@@ -63,7 +59,7 @@
                         <ul>
                             <li><em class="gras">$this</em> fait référence à l'objet qui a appelé la méthode</li>
                             <li><em class="gras">Une méthode</em> est une fonction à l'intérieur d'une classe</li>
-                            <li><em class="gras"></em></li>
+                            <li><em class="gras">L'encapsulation</em> désigne le principe de regrouper des données avec un ensemble de méthodes permettant de les lire ou de les manipuler, empêchant l'accès aux données par un autre moyen que les services proposés. L'encapsulation permet donc de garantir l'intégrité des données contenues dans l'objet</li>
                             <li><em class="gras"></em></li>
                             <li><em class="gras"></em></li>
                             <li><em class="gras"></em></li>
@@ -78,12 +74,18 @@
                             <li><em class="gras">$objet = new class();</em> permet de créer un objet à partir d'une classe</li>
                             <li><em class="gras">$objet->propriété = valeur;</em> permet d'attribuer une valeur à une propriété</li>
                             <li><em class="gras">$objet->méthode(paramètre);</em> permet d'appeler une méthode sur un objet</li>
-                            <li><em class="gras"></em></li>
-                            <li><em class="gras"></em></li>
+                            <li><em class="gras">private</em> contrairement à public, private vérouille l'accès aux propriétés et méthodes qui sont dans un objet (elles sont accessibles uniquement depuis le contexte interne de l'objet). On utilise plutôt private pour les propriétés et public pour les fonctions</li>
+                            <li><em class="gras">$objet->fonction();</em> permet d'appeler une méthode publique</li>
                             <li><em class="gras"></em></li>
                         </ul>
+            </article>
 
-                   
+        </section>
+
+        <section id="second-section">
+
+            <article class="topic">   
+
                 <h2>Exemples utiles</h2>
 
                     <h3>Exemple d'utilisation de __construct</h3>
@@ -99,8 +101,67 @@
                         <p>public function credit($amount) {</p>
                         <p>$this->balance += $amount;</p>
                         <p>}</p>
+
+                    <h3>Exemple de définition d'une classe</h3>
+
+                        <p>class MyClass</p>
+                        <p>{</p>
+                        <p>private $property;</p>
+                        <p>public function myMethod()</p>
+                        <p>{</p>
+                        <p>...</p>
+                        <p>}</p>
+                        <p>}</p>
                         <p></p>
                         <p></p>
+
+                    <h3>Exemple de getter/setter manuels</h3>
+
+                        <p>class Person {</p>
+                        <p>private $secret = 123</p>
+                        <p>public function getSecret(</p>
+                        <p>{</p>
+                        <p>return $this->secret;</p>
+                        <p>}</p>
+                        <p>public function setSecret($newSecret)</p>
+                        <p>{</p>
+                        <p>if(is_int($newSecret)) {</p>
+                        <p>$this->secret = $newSecret;</p>
+                        <p>}</p>
+                        <p>}</p>
+                        <p>}</p>
+                    
+                    <h3>Exemple de getter/setter avec méthode PHP</h3>
+
+                        <p>class Person</p>
+                        <p>{</p>
+                        <p>private $secretCode = 123;</p>
+                        <p>private $age;</p>
+                        <p>public function __get($property)</p>
+                        <p>{</p>
+                        <p>if( "age" == $property) {</p>
+                        <p>return $this->age;</p>
+                        <p>}</p>
+                        <p>elseif ("secretCode" == $property) {</p>
+                        <p>echo "propriété privée";</p>
+                        <p>}</p>
+                        <p>else {</p>
+                        <p>echo "propriété n'existe pas";</p>
+                        <p>}</p>
+                        <p>}</p>
+                        <p>public function __set($property, $value)</p>
+                        <p>{</p>
+                        <p>if("age" == $property && is_int($value)) {</p>
+                        <p>$this->age = $value;</p>
+                        <p>}</p>
+                        <p>elseif ("secretCode" == $property) {</p>
+                        <p>echo "c'est mon secret, pas touche."</p>
+                        <p>}</p>
+                        <p>else {</p>
+                        <p>echo "propriété ou valeur invalide";</p>
+                        <p>}</p>
+                        <p>}</p>
+                        <p>}</p>
                         <p></p>
                     
             </article>
