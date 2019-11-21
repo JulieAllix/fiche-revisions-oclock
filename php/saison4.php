d08b045dd3c5a35a846c72d81e6798a73332ff93
JulieAllix <allixjulie@gmail.com>
2019-11-21 20:16:41 +0100

    ajout de notions du 21 11

--- php/saison4.php
+++ php/saison4.php
@@ -19,6 +19,26 @@
 
             <article class="topic">
 
+                    <h2>En vrac</h2>
+
+                        <h3>Bon à savoir</h3>
+
+                            <ul>
+                                <li><em class="gras">Network</em> dans l'inspecteur de Chrome permet de voir s'il manque des éléments qui ont été appelés dans le code (img par ex)</li>
+                                <li><em class="gras"></em></li>
+                                <li><em class="gras"></em></li>
+                                <li><em class="gras"></em></li>
+                                <li><em class="gras"></em></li>
+                                <li><em class="gras"></em></li>
+                                <li><em class="gras"></em></li>
+                                <li><em class="gras"></em></li>
+                                <li><em class="gras"></em></li>
+                            </ul>
+
+            </article>
+
+            <article class="topic">
+
                 <h2>Git : branches</h2>
 
                     <h3>Bon à savoir</h3>
@@ -43,8 +63,10 @@
                             <li><em class="gras">git fetch</em> récupérer les modifs d'un repo sur Github, mais ne permet pas de faire de modifs (n'écrase pas la version précédente en local)</li>
                             <li><em class="gras">git checkout nomDeLaBranche</em> permet de basculer sur une branche/récupérer une branche</li>
                             <li><em class="gras">git branch</em> montre les branches qui existent (l'étoile indique celle sur laquelle je suis)</li>
-                            <li><em class="gras">git merge nomDeLaBranche</em> fusionne la branche avec master</li>
+                            <li><em class="gras">git merge nomDeLaBranche</em> fusionne la branche avec master (il faut d'abord se positionner sur la branche master avec git checkout master)</li>
                             <li><em class="gras">git branch -a</em> permet de voir les branches dispo en local et celles dispo sur Github (en rouge)</li>
+                            <li><em class="gras">git push -u origin nom-de-la-branche</em> permet de pusher une branche nouvellement créée</li>
+                            <li><em class="gras"></em></li>
                             <li><em class="gras"></em></li>
                         </ul>
 
@@ -60,7 +82,7 @@
                             <li><em class="gras">$this</em> fait référence à l'objet qui a appelé la méthode</li>
                             <li><em class="gras">Une méthode</em> est une fonction à l'intérieur d'une classe</li>
                             <li><em class="gras">L'encapsulation</em> désigne le principe de regrouper des données avec un ensemble de méthodes permettant de les lire ou de les manipuler, empêchant l'accès aux données par un autre moyen que les services proposés. L'encapsulation permet donc de garantir l'intégrité des données contenues dans l'objet</li>
-                            <li><em class="gras"></em></li>
+                            <li>Il est possible de comparer deux fichiers dans VSC : il faut les sélectionner puis <em class="gras">clic droit, comparer</em></li>
                             <li><em class="gras"></em></li>
                             <li><em class="gras"></em></li>
                         </ul>
@@ -76,6 +98,8 @@
                             <li><em class="gras">$objet->méthode(paramètre);</em> permet d'appeler une méthode sur un objet</li>
                             <li><em class="gras">private</em> contrairement à public, private vérouille l'accès aux propriétés et méthodes qui sont dans un objet (elles sont accessibles uniquement depuis le contexte interne de l'objet). On utilise plutôt private pour les propriétés et public pour les fonctions</li>
                             <li><em class="gras">$objet->fonction();</em> permet d'appeler une méthode publique</li>
+                            <li><em class="gras">str-replace('ce que je veux remplacer','par quoi', élément où l'appliquer)</em> permet de remplacer une string</li>
+                            <li><em class="gras"></em></li>
                             <li><em class="gras"></em></li>
                         </ul>
             </article>
@@ -168,11 +192,14 @@
 
             <article class="topic">
 
-                <h2></h2>
+                <h2>SQL, BDD</h2>
 
-                    <h3></h3>
+                    <h3>Phpmyadmin</h3>
 
                         <ul>
+                            <li><em class="gras">http://localhost/phpmyadmin/</em> permet d'accéder à PMA</li>
+                            <li><em class="gras"></em></li>
+                            <li><em class="gras"></em></li>
                             <li><em class="gras"></em></li>
                         </ul>
                     
