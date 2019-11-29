c49b247e1be1ab7bc56b1d53f2482b29578e8726
JulieAllix <allixjulie@gmail.com>
2019-11-29 16:56:27 +0100

    ajout des notions du 29 11

--- php/saison5.php
+++ php/saison5.php
@@ -51,6 +51,49 @@
 
             </article> 
 
+            <article class="topic">
+
+                <h2>Composer</h2>
+
+                    <h3>Les packages/dépendances</h3>
+
+                        <ul>
+                            <li>Les packages s'installent à la racine de notre repo</li>
+                            <li>Quand on a un projet avec un composer.json, toujours faire un <em class="gras">composer install</em> dans le terminal pour récupérer les dépendances</li>
+                            <li><em class="gras">composer update</em> permet de faire des màj dans le repo</li>
+                            <li></li>
+                            <li></li>
+                            <li><em class="gras">symfony/var_dumper</em> permet d'afficher les var_dump de façon + clean. On utilise le raccourci dump.</li>
+                            <li><em class="gras">altorouter</em> permet d'aller chercher des paramètres d'url dynamiques</li>
+                            <li><em class="gras">$router->setBasePath($_SERVER['BASE_URI']);</em> sert à indiquer où se trouve la racine de mon site. Il faut le placer dans l'index, en général.</li>
+                            <li><em class="gras">$router->map(méthode, url, data, id de route)</em> permet d'ajouter une route</li>
+                            <li>par ex : $router->map('GET', '/', 'render-home', 'home')</li>
+                            <li><em class="gras">$match = $router->match();</em> permet de vérifier s'il y a une route correspondant à la page demandée</li>
+                            <li><em class="gras">vendor/autoload.php</em> permet de charger toutes les dépendances. Il faut donc le require pour les charger dans un fichier donné.</li>
+                            
+                            <li><em class="gras"></em></li>
+                            <li><em class="gras"></em></li>
+                            <li><em class="gras"></em></li>
+                            <li><em class="gras"></em></li>
+                            <li><em class="gras"></em></li>
+                        </ul>
+
+                    <h3></h3>
+
+                        <ul>
+                            <li><em class="gras"></em></li>
+                            <li><em class="gras"></em></li>
+                            <li><em class="gras"></em></li>
+                            <li><em class="gras"></em></li>
+                            <li><em class="gras"></em></li>
+                            <li><em class="gras"></em></li>
+                            <li><em class="gras"></em></li>
+                            <li><em class="gras"></em></li>
+                            <li><em class="gras"></em></li>
+                        </ul>
+
+            </article> 
+
 
         
         </section>
@@ -67,6 +110,14 @@
                             <li><em class="gras">.htaccess</em> donne des instructions/directives à Apache</li>
                             <li><em class="gras">Deny from all</em> : tous les fichiers du répertoire et les sous-dossiers sont interdits</li>
                             <li><em class="gras">$_GET['_url']</em> permet de récupérer l'information sur la page demandée suite à la mise en place du htaccess de redirection</li>
+                            <li><em class="gras"></em></li>
+                            <li><em class="gras"></em></li>
+                            <li><em class="gras"></em></li>
+                            <li><em class="gras"></em></li>
+                            <li><em class="gras"></em></li>
+                            <li><em class="gras"></em></li>
+                            <li><em class="gras"></em></li>
+                            <li><em class="gras"></em></li>
                         </ul>
 
                    
@@ -90,9 +141,18 @@
                             <li></li>
                         </ul>
                     
-                    <h3></h3>
+                    <h3>Architecture MVC : Model - View - Controller</h3>
 
                         <ul>
+                            <li><em class="gras">Model</em>: c'est une couche qui correspond à l'ensemble des classes définissant les objets manipulés par l'application, qui contiennent les données et réalisent les opérations de stockage. Le Model protège l'intégrité des données en implémentant la logique métier et s'occupe du stockage.</li>
+                            <li><em class="gras">Controller</em>: gère l'aspect dynamique de l'application. A partir de la requête utilisateur, il récupère les données grâce au Model, les injecte dans la View correspondante, et envoie la réponse produite.</li>
+                            <li><em class="gras">Dispatcher</em>: instancie le Controller et exécute l'action.</li>
+                            <li><em class="gras"></em></li>
+                            <li><em class="gras"></em></li>
+                            <li><em class="gras"></em></li>
+                            <li><em class="gras"></em></li>
+                            <li><em class="gras"></em></li>
+                            <li><em class="gras"></em></li>
                             <li><em class="gras"></em></li>
                             <li><em class="gras"></em></li>
                         </ul>
