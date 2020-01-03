e7b5393ce9ea44ee215842cce38e8d00a2a4cfcd
JulieAllix <allixjulie@gmail.com>
2020-01-03 17:03:35 +0100

    notions du 3/1

--- php/saison7.php
+++ php/saison7.php
@@ -27,10 +27,10 @@
                             <li>Lumen est un micro-framework de Laravel</li>
                             <li>Lumen a son propre router</li>
                             <li>Dans la doc de Lumen, il faut faire attention à consulter la doc de la bonne version (nous avons installé la v6)</li>
+                            <li>On peut appeler par exemple un header en faisant <em class="gras">< ?= view('layout.header'); ?></em></li>
                             <li></li>
                             <li></li>
-                            <li></li>
-                            <li><em class="gras"></em></li>
+                            <li><em class="gras">Blade (https://laravel.com/docs/6.x/blade)</em> permet de simplifier le mélange de code php avec html (mais ajoute une nouvelle syntaxe)</li>
                             <li><em class="gras"></em></li>
                             <li><em class="gras"></em></li>
                             <li><em class="gras"></em></li>
@@ -55,6 +55,17 @@
                             <li><em class="gras"></em></li>
                             <li><em class="gras"></em></li>
                         </ul>
+
+                        <h3>DB</h3>
+
+                            <ul>
+                                <li>Pour se connecter à une DB, il suffit d'aller dans le fichier <em class="gras">.env</em> et de remplir les <em class="gras">DB_DATABASE, DB_USERNAME et DB_PASSWORD</em></li>
+                                <li></li>
+                                <li></li>
+                                <li>Pour faire une requête SQL dans une page, il faut penser à faire l'appel <em class="gras">use Illuminate\Support\Facades\DB;</em></li>
+                                <li><em class="gras">$tableList = DB::select('SELECT * FROM `table`');</em> permet de récupérer les données d'une table</li>
+                                <li>On peut aussi faire <em class="gras">$tableList = Table::all();</em> pour récupérer les données d'une table</li>
+                            </ul>
 
             </article> 
 
@@ -89,18 +100,36 @@
 
             <article class="topic">
 
-                <h2></h2>
+                <h2>Eloquent</h2>
 
-                    <h3></h3>
+                    <h3>Good to know</h3>
 
                         <ul>
+                            <li>Si notre DB ne respecte pas les conventions de nommage d'Eloquent, il faut venir le préciser, par exemple :</li>
+                            <p>class Videogame extends Model</p>
+                            <p>{</p>
+                            <p>protected $table = 'videogame';</p>
+                            <p>public $timestamps = false;</p>
+                            <p>}</p>
+                            <p></p>
+                            <li></li>
+                            <li><em class="gras"></em></li>
                             <li><em class="gras"></em></li>
                         </ul>
-                    
-                    <h3></h3>
+
+                    <h3>Models</h3>
 
                         <ul>
-                            <li><em class="gras"></em></li>
+                            <li>Pour définir un nouveau Model, il faut faire un <em class="gras">use Illuminate\Database\Eloquent\Model;</em></li>
+                            <li>Et il faut aussi faire un <em class="gras">extends Model</em> à notre nouvelle classe</li>
+                        </ul>
+                    
+                    <h3>Exemples de conventions de nommage d'Eloquent</h3>
+
+                        <ul>
+                            <li><em class="gras">Table names:</em> snake case plural (ex: videogames)</li>
+                            <li><em class="gras">Primary key:</em> id en nom de colonne</li>
+                            <li><em class="gras">Timestamps:</em> Eloquent s'attend à trouver des colonnes created_at et updated_at</li>
                             <li><em class="gras"></em></li>
                         </ul>
     
