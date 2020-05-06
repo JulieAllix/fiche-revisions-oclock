c12a7ce79899550ba838df8b7aad4af020360468
JulieAllix <allixjulie@gmail.com>
2020-05-06 17:27:38 +0200

    Notions du 6/5

---
+++ php/javascript-tutorials.php
@@ -0,0 +1,101 @@
+<?php require 'partials/head.php'; ?>
+
+<body>
+
+<?php // require 'partials/logo.php'; ?>
+<?php require 'partials/toggleSwitch.php'; ?>
+<script src="../js/toggleSwitch.js"></script>
+
+    <header>
+
+    <h1>Fiche de révisions : Tutoriels - JavaScript</h1>
+    <?php require 'partials/main-nav-bar.php'; ?>
+
+    </header>
+
+    <main id="wrapper">
+
+        <section id="first-section">
+
+            <article class="topic">
+
+                <h2>Smooth Page transitions</h2>
+
+                    <h3>Installations</h3>
+
+                        <ul>
+                            <li>Vériier que Node est installé<em class="gras"></em></li>
+                            <li><em class="gras">npm init</em> (presser enter à chaque question)</li>
+                            <li><em class="gras">npm install -g parcel-bundler</em></li>
+                            <li><em class="gras">parcel index.html</em> va créer un serveur</li>
+                            <li>Les packages qui vont servir à faire les animations sont :<em class="gras"></em></li>
+                            <li><em class="gras">npm install gsap</em></li>
+                            <li><em class="gras">npm install @dogstudio/highway</em></li>
+                            <li><em class="gras"></em></li>
+                            <li><em class="gras"></em></li>
+                            <li><em class="gras"></em></li>
+                        </ul>
+
+                    <h3></h3>
+
+                        <ul>
+                            <li><em class="gras"></em></li>
+                            <li><em class="gras"></em></li>
+                            <li><em class="gras"></em></li>
+                            <li><em class="gras"></em></li>
+                            <li><em class="gras"></em></li>
+                            <li><em class="gras"></em></li>
+                            <li><em class="gras"></em></li>
+                            <li><em class="gras"></em></li>
+                            <li><em class="gras"></em></li>
+                        </ul>
+
+            </article> 
+
+
+        
+        </section>
+
+        <section id="second-section">
+
+            <article class="topic">
+
+                <h2></h2>
+
+                    <h3></h3>
+
+                        <ul>
+                            <li><em class="gras"></em></li>
+                        </ul>
+
+                   
+                    <h3></h3>
+                        <p></p>
+                        <p></p>
+                    
+            </article>
+
+            <article class="topic">
+
+                <h2></h2>
+
+                    <h3></h3>
+
+                        <ul>
+                            <li><em class="gras"></em></li>
+                        </ul>
+                    
+                    <h3></h3>
+
+                        <ul>
+                            <li><em class="gras"></em></li>
+                            <li><em class="gras"></em></li>
+                        </ul>
+    
+            </article>
+
+        </section>
+
+    </main>
+
+<?php require 'partials/footer.php'; ?>
